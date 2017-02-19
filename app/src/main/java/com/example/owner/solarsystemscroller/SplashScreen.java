a39db8a185d3476e2b2d1a2e6ca590ae660cd109
Tiara Tan <tt1506@nyu.edu>
2017-02-19 06:07:11 -0500

    Added SplashScreen

---
+++ app/src/main/java/com/example/owner/solarsystemscroller/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.example.owner.solarsystemscroller;
+
+/**
+ * Created by owner on 2/19/2017.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
